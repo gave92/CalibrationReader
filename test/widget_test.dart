8a99bccdc36eabc96049ea59075421d3dde23575
Marco Gavelli <marcogavelli2@gmail.com>
2024-03-12 21:43:00 +0100

    Updated test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -16,15 +16,10 @@ void main() {
     await tester.pumpWidget(const CalRearder(args: []));
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.byIcon(Icons.add_outlined), findsOneWidget);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    //await tester.tap(find.byIcon(Icons.add_outlined));
+    //await tester.pump();
   });
 }
